2af3682ba69f05d7fa7e617fd14ab3599174e538
Felix A. Schultz <felix.schultz@engapho.com>
2024-01-22 16:23:45 +0100

    Add /crawl endpoint to fetch headers from a given URL

--- crawler.js
+++ crawler.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import cors from 'cors';
+import axios from 'axios';
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.get("/crawl", async (req, res) => {
+    const url = "https://" + req.query.url;
+    
+    try{
+        const response = (await axios.get(url));
+        res.status(200).send(response.headers);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
 
-app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+app.listen(3002, () => {
+    console.log('Server is listening on port 3002');
 });
