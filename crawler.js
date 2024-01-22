c468e4d0dbb4c1f67deea5b31ecf9eace3b3d541
Felix A. Schultz <felix.schultz@engapho.com>
2024-01-22 20:01:00 +0100

    Refactor crawler.js to handle different crawl endpoints

--- crawler.js
+++ crawler.js
@@ -6,17 +6,35 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get("/crawl", async (req, res) => {
-    const url = "https://" + req.query.url;
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url);
-    const cookies = await page.cookies();
-    const title = await page.title();
-    await browser.close();
-    console.log(title);
-    res.status(200).json(cookies);
+app.get("/crawl-cookie", async (req, res) => {
+    try{
+        const url = "http://" + req.query.url;
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: 'load', timeout: 0 });
+        const cookies = await page.cookies();
+        await browser.close();
+        res.status(200).json(cookies);
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
 });
+
+app.get("/crawl", async (req, res) => {
+    try{
+        const url = "http://" + req.query.url;
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(url, { waitUntil: 'load', timeout: 0 });
+        const content = await page.content();
+        await browser.close();
+        res.status(200).send(content);
+    }
+    catch(err){
+        res.status(500).json({message: err.message});
+    }
+})
 
 app.listen(3003, () => {
     console.log('Server is listening on port https//localhost:3003');
