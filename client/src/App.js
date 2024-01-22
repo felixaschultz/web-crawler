2ebf0de1e8dd27a82c82fed3aa654d0d36ac3a2c
Felix A. Schultz <felix.schultz@engapho.com>
2024-01-22 16:23:54 +0100

    Add initial project files

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [url, setUrl] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    fetch(`http://localhost:3002/crawl?url=${url}`)
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById('website').innerHTML = data;
+      });
+  }
+
+  return (
+    <>
+      <h1>Webcrawler</h1>
+      <form>
+        <label>
+          URL:
+          <input type="text" name="url" onChange={(e) => setUrl(e.target.value)} />
+        </label>
+        <button type="submit" onClick={handleSubmit}>Crawl</button>
+      </form>
+      <div id="website"></div>
+    </>
+  );
+}
+
+export default App;
