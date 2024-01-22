1dceebe063bbad78dda971eadabafdf7e994dfae
Felix A. Schultz <felix.schultz@engapho.com>
2024-01-22 20:01:10 +0100

    Add type selection for crawling and display loading message

--- client/src/App.js
+++ client/src/App.js
@@ -3,23 +3,29 @@ import { useState } from 'react';
 
 function App() {
   const [url, setUrl] = useState('');
+  const [type, setType] = useState('cookie');
 
   function handleSubmit(e) {
     e.preventDefault();
-
-    fetch(`http://localhost:3003/crawl?url=${url}`)
+    const website = document.getElementById('website');
+    document.getElementById('website').innerHTML = '<h2 style="grid-column: 1/3">Loading...</h2>';
+    if(type === 'cookie'){
+      fetch(`http://localhost:3003/crawl-cookie?url=${url}`)
       .then(res => res.json())
       .then(data => {
-        const website = document.getElementById('website');
-        website.innerHTML = `<h2>${url}</h2>`;
-        data.forEach(cookie => {
+        if(data.message){
+          website.innerHTML = `<h2 style="grid-column: 1/3">${data.message}</h2>`;
+          return;
+        }
+        website.innerHTML = `<h2 style="grid-column: 1/3">${url}</h2>`;
+        data?.forEach(cookie => {
           website.innerHTML += `
             <div>
               <h3>${cookie.name}</h3>
               <p>Value: ${cookie.value}</p>
               <p>Domain: ${cookie.domain}</p>
               <p>Path: ${cookie.path}</p>
-              <p>Expires: ${cookie.expires}</p>
+              <p>Expires: ${new Date(cookie.expires).toUTCString()}</p>
               <p>Size: ${cookie.size}</p>
               <p>HttpOnly: ${cookie.httpOnly}</p>
               <p>Secure: ${cookie.secure}</p>
@@ -27,6 +33,14 @@ function App() {
             </div>`;
         });
       });
+    }else if(type === 'content'){
+      fetch(`http://localhost:3003/crawl?url=${url}`)
+      .then(res => res.text())
+      .then(data => {
+        const website = document.getElementById('website');
+        website.innerHTML = data;
+      });
+    }
   }
 
   return (
@@ -37,9 +51,16 @@ function App() {
           URL:
           <input type="text" name="url" onChange={(e) => setUrl(e.target.value)} />
         </label>
+        <label>
+          Type:
+          <select onChange={(e) => setType(e.target.value)}>
+            <option value="cookie">Cookie</option>
+            <option value="content">Content</option>
+          </select>
+        </label>
         <button type="submit" onClick={handleSubmit}>Crawl</button>
       </form>
-      <div id="website"></div>
+      <div id="website" style={{display: "grid", gridTemplateColumns: "1fr 1fr"}}></div>
     </>
   );
 }
