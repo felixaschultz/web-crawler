28cfa6431d2185999b3e32d5e725faaafcd254ba
Felix A. Schultz <felix.schultz@engapho.com>
2024-01-25 14:47:54 +0100

    Add loading spinner and display cookies

--- client/src/App.js
+++ client/src/App.js
@@ -1,55 +1,40 @@
 import './App.css';
+import LoadingSpinner from './components/loading/loading';
 import { useState } from 'react';
 
 function App() {
   const [url, setUrl] = useState('');
   const [type, setType] = useState('cookie');
   const [allCookies, setAllCookies] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [cookies, setCookies] = useState();
 
   function handleSubmit(e) {
     e.preventDefault();
-    const website = document.getElementById('website');
-    document.getElementById('website').innerHTML = '<h2 style="grid-column: 1/3">Loading...</h2>';
+    setLoading(true);
     if(type === 'cookie'){
       fetch(`http://localhost:3003/crawl?url=${url}&acceptAll=${allCookies}`)
       .then(res => res.json())
       .then(data => {
-        if(data.message){
-          website.innerHTML = `<h2 style="grid-column: 1/3">${data.message}</h2>`;
-          return;
-        }
-        website.innerHTML = `<h2 style="grid-column: 1/2">${url}</h2>`;
-        website.innerHTML += `<p style="grid-column: 2/3">Found ${data.length} cookies</p>`;
-        
-        data?.forEach(cookie => {
-          const date = new Date(cookie.expires * 1000);
-          const year = date.getFullYear();
-          const month = date.getMonth() + 1;
-          const day = date.getDate();
-          const expires = `${day}. ${month} ${year}`;
-          website.innerHTML += `
-            <div>
-              <h3>${cookie.name}</h3>
-              <p>Value: ${cookie.value}</p>
-              <p>Domain: ${cookie.domain}</p>
-              <p>Path: ${cookie.path}</p>
-              <p>Expires: ${expires}</p>
-              <p>Size: ${cookie.size}</p>
-              <p>HttpOnly: ${cookie.httpOnly}</p>
-              <p>Secure: ${cookie.secure}</p>
-              <p>Session: ${cookie.session}</p>
-            </div>`;
-        });
+        setCookies(data);
+      }).finally(() => {
+        setLoading(false);
       });
     }else if(type === 'content'){
       fetch(`http://localhost:3003/crawl/site?url=${url}`)
       .then(res => res.text())
       .then(data => {
-        const website = document.getElementById('website');
-        website.innerHTML = data;
+        
       });
     }
   }
+
+ /*  if(!loading){
+    console.log(cookies)
+    cookies.forEach(cookie => {
+      console.log(cookie);
+    })
+  } */
 
   return (
     <>
@@ -67,12 +52,31 @@ function App() {
           </select>
         </label>
         <label>
-          Accept All:
+          Accept all cookies:
           <input type="checkbox" name="acceptAll" onChange={(e) => setAllCookies(e.target.checked)} />
         </label>
         <button type="submit" onClick={handleSubmit}>Crawl</button>
       </form>
-      <div id="website"></div>
+      {(!loading) ? cookies?.map(cookie => {
+          const date = new Date(cookie.expires * 1000);
+          const year = date.getFullYear();
+          const month = date.getMonth() + 1;
+          const day = date.getDate();
+          const expires = `${day}. ${month} ${year}`;
+          return <>
+            <div>
+              <h3>{cookie.name}</h3>
+              <p>Value: {cookie.value}</p>
+              <p>Domain: {cookie.domain}</p>
+              <p>Path: {cookie.path}</p>
+              <p>Expires: {expires}</p>
+              <p>Size: {cookie.size}</p>
+              <p>HttpOnly: {cookie.httpOnly}</p>
+              <p>Secure: {cookie.secure}</p>
+              <p>Session: {cookie.session}</p>
+            </div>
+            </>;
+        }) : <LoadingSpinner />}
     </>
   );
 }
---
+++ client/src/components/loading/loading.js
@@ -0,0 +1,8 @@
+import "./Style.css";
+export default function LoadingSpinner(){
+    return <>
+        <div className="loading-spinner">
+            <div className="spinner"></div>
+        </div>
+    </>
+}
